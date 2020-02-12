c8dc2931f1fec77bc0a72f9671c7e25b4f8ebaed
David Brandt <brandt.de@gmail.com>
2020-02-11 23:22:44 -0700

    Initial commit

---
+++ src/renderer/components/App.tsx
@@ -0,0 +1,10 @@
+import React, { FunctionComponent } from 'react'
+
+const App: FunctionComponent = (): JSX.Element => {
+
+    return (
+        <div>Online Course Scheduler</div>
+    )
+}
+
+export default App
